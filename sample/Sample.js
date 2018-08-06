019e495c345d4a3fe6228743178598230e1f172a
Wandalen <wandalen.me@gmail.com>
2018-08-06 11:04:07 +0300

    automated commit

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,9 +1,9 @@
 
 if( typeof module !== 'undefined' )
-require( 'wTools' );
+require( 'wPath' );
+
 var _ = wTools;
 
-/**/
-
-...
-
+var pathFile = '/a/b/c.x'
+var name = _.pathName( pathFile );
+console.log( 'name of ' + pathFile + ' is ' + name );
