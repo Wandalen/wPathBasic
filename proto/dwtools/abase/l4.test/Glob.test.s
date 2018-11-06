2bd88e207bba14881f0405f142325b9e370f4f7a
Wandalen <wandalen.me@gmail.com>
2018-11-06 16:57:53 +0200

    auto

--- proto/dwtools/abase/l4.test/Glob.test.s
+++ proto/dwtools/abase/l4.test/Glob.test.s
@@ -647,19 +647,19 @@ var Self =
 
 Self = wTestSuite( Self );
 if( typeof module !== 'undefined' && !module.parent )
-/*_.*/wTester.test( Self.name );
+wTester.test( Self.name );
 
 if( 0 )
 if( typeof module === 'undefined' )
 _.timeReady( function()
 {
 
-  /*_.*/wTester.verbosity = 99;
-  /*_.*/wTester.logger = wPrinterToJs({ outputGray : 0, writingToHtml : 1 });
-  /*_.*/wTester.test( Self.name,'PathUrlTest' )
+  wTester.verbosity = 99;
+  wTester.logger = wPrinterToJs({ outputGray : 0, writingToHtml : 1 });
+  wTester.test( Self.name,'PathUrlTest' )
   .doThen( function()
   {
-    var book = /*_.*/wTester.loggerToBook();
+    var book = wTester.loggerToBook();
   });
 
 });
