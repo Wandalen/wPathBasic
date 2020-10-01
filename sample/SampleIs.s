a6f26d3f43ca6c3af2327f07469c20d225138870
Jack Shell <jackshel45@gmail.com>
2020-10-01 14:11:18 +0300

    Refactored file "SampleIs.s"

--- sample/SampleIs.s
+++ sample/SampleIs.s
@@ -8,25 +8,28 @@ let _ = wTools;
 // Refine
 
 var path = '../../foo/bar/';
-var got = _.path.isRefined( path ); // returns true when the path has slash ( / ) in the end .
+var got = _.path.isRefined( path ); 
+/* returns true when the path has slash ( / ) in the end . */
 
 logger.log( 'isRefined', got);
 
 var path = '../../foo/bar';
-var got = _.path.isRefined( path ); // checks that the string doesn´t contain left( \\ ) or double slashes ( // ) ).
-
+var got = _.path.isRefined( path ); 
+/* checks that the string doesn´t contain left( \\ ) or double slashes ( // ) ). */
 logger.log( 'is Refined without trail', got);
 logger.log( '' );
 
 // Normalize
 
 var path = '../../foo/bar/';
-var got = _.path.isNormalized( path ); // checks that pass is trailed
+var got = _.path.isNormalized( path );
+/* checks that pass is trailed */
 
 logger.log( 'isNormalized', got);
 
 var path = '../../foo/bar';
-var got = _.path.isNormalized( path ); // checks that pass is normalized( collapsed redundant delimeters and resolved '..' and '.' segments )
+var got = _.path.isNormalized( path );
+/* checks that pass is normalized( collapsed redundant delimeters and resolved '..' and '.' segments ) */
 
 logger.log( 'is Normalized without trail', got);
 logger.log( '' );
