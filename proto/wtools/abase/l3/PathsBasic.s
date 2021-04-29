76b09459c41deccc75a0e0d89782b991e1acfe31
wandalen <wandalen.me@gmail.com>
2021-04-29 18:16:28 +0300

    .

--- proto/wtools/abase/l3/PathsBasic.s
+++ proto/wtools/abase/l3/PathsBasic.s
@@ -528,7 +528,7 @@ Parent.Init = function Init()
 {
   let result = OriginalInit.apply( this, arguments );
 
-  _.assert( _.objectIs( this.s ) );
+  _.assert( _.object.isBasic( this.s ) );
   _.assert( this.s.single !== undefined );
   this.s = Object.create( this.s );
   this.s.single = this;
